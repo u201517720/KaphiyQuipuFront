f5ae3899c798b53696ae7520b6ed4ee562c79d1f
Jordan Dávila Terrones <jjordandt@gmail.com>
2022-01-12 18:25:59 -0500

    server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/kaphiyquipu'));
 
 app.get('*', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + '/kaphiyquipu/dist/KaphiyQuipuFront'});
+    res.sendFile('index.html', {root: __dirname });
 });
 
 app.listen(process.env.PORT || 8080);
