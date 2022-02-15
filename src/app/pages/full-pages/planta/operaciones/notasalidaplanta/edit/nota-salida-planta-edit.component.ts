08ff7fabe616de2ebc4ebf60c545121d1a1b8632
Jordan Dávila Terrones <u201517720@upc.edu.pe>
2022-02-14 23:52:47 -0500

    documento de pago hacia el distribuidor

--- src/app/pages/full-pages/planta/operaciones/notasalidaplanta/edit/nota-salida-planta-edit.component.ts
+++ src/app/pages/full-pages/planta/operaciones/notasalidaplanta/edit/nota-salida-planta-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -6,11 +6,14 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { GuiaremisionplantaService } from '../../../../../../Services/guiaremisionplanta.service';
 import { AlertUtil } from '../../../../../../Services/util/alert-util';
 import { NotasalidaplantaService } from '../../../../../../Services/notasalidaplanta.service';
+import { MaestroService } from '../../../../../../Services/maestro.service';
+import { ContratoService } from '../../../../../../Services/contrato.service';
 
 @Component({
   selector: 'app-nota-salida-planta-edit',
   templateUrl: './nota-salida-planta-edit.component.html',
-  styleUrls: ['./nota-salida-planta-edit.component.scss']
+  styleUrls: ['./nota-salida-planta-edit.component.scss', '/assets/sass/libs/datatables.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NotaSalidaPlantaEditComponent implements OnInit {
 
@@ -20,12 +23,18 @@ export class NotaSalidaPlantaEditComponent implements OnInit {
     private spinner: NgxSpinnerService,
     private fb: FormBuilder,
     private router: Router,
+    private maestroService: MaestroService,
+    private contratoService: ContratoService,
     private notasalidaplantaService: NotasalidaplantaService) { }
 
   locId = 0;
   userSession: any;
   mensajeGenerico = 'Ha ocurrido un error interno. Por favor, comuníquese con el área de soporte de sistemas.';
   frmNotaSalidaPlantaDetalle: FormGroup;
+  locEstado = 0;
+  limitTrans = 10;
+  selectedTrans = [];
+  rowsTrans = [];
 
   ngOnInit(): void {
     this.locId = this.route.snapshot.params['id'] ? parseInt(this.route.snapshot.params['id']) : 0;
@@ -48,8 +57,6 @@ export class NotaSalidaPlantaEditComponent implements OnInit {
       rucTransformador: [],
       destinatario: [],
       destino: [],
-      transportista: [],
-      vehiculo: [],
       correlativoNIP: [],
       producto: [],
       nroContrato: [],
@@ -117,6 +124,7 @@ export class NotaSalidaPlantaEditComponent implements OnInit {
 
   async CompletarFormulario(data) {
     if (data) {
+      this.locEstado = data.EstadoId;
       this.frmNotaSalidaPlantaDetalle.controls.correlativo.setValue(data.CorrelativoNSP);
       this.frmNotaSalidaPlantaDetalle.controls.fechaRegistro.setValue(data.FechaRegistro);
       this.frmNotaSalidaPlantaDetalle.controls.transformadora.setValue(data.Transformadora);
@@ -124,8 +132,6 @@ export class NotaSalidaPlantaEditComponent implements OnInit {
       this.frmNotaSalidaPlantaDetalle.controls.rucTransformador.setValue(data.RucTransformadora);
       this.frmNotaSalidaPlantaDetalle.controls.destinatario.setValue(data.Acopiador);
       this.frmNotaSalidaPlantaDetalle.controls.destino.setValue(data.DireccionAcopiador);
-      this.frmNotaSalidaPlantaDetalle.controls.transportista.setValue(data.Conductor);
-      this.frmNotaSalidaPlantaDetalle.controls.vehiculo.setValue(data.PlacaTractor);
       this.frmNotaSalidaPlantaDetalle.controls.correlativoNIP.setValue(data.CorrelativoNIP);
       this.frmNotaSalidaPlantaDetalle.controls.producto.setValue(data.Producto);
       this.frmNotaSalidaPlantaDetalle.controls.nroContrato.setValue(data.CorrelativoContrato);
@@ -138,11 +144,88 @@ export class NotaSalidaPlantaEditComponent implements OnInit {
       this.frmNotaSalidaPlantaDetalle.controls.tara.setValue(data.CafeExportacionSacos * 0.3);
       this.frmNotaSalidaPlantaDetalle.controls.kilosNetos.setValue(data.CafeExportacionSacos * data.PesoSaco);
       this.frmNotaSalidaPlantaDetalle.controls.certificacion.setValue(`${data.SubProducto} - ${data.Certificacion}`);
+      this.ActualizarListaTransportistas();
     }
     this.spinner.hide();
   }
 
   Cancelar() {
     this.router.navigate(['/planta/operaciones/notasalida/list']);
+  }
+
+  updateLimit(e) {
+    this.limitTrans = e.target.value;
+  }
+
+  ActualizarListaTransportistas() {
+    this.spinner.show();
+    this.rowsTrans = [];
+    if (this.locEstado === 1) {
+      this.maestroService.ConsultarTransportista({ Codigo: 'TransporteContratoPlantaSalida' })
+        .subscribe((res) => {
+          this.spinner.hide();
+          if (res.Result.Success) {
+            this.rowsTrans = res.Result.Data;
+          } else {
+            this.alertUtil.alertError('ERROR', res.Result.Message);
+          }
+        }, (err) => {
+          this.spinner.hide();
+          this.alertUtil.alertError('ERROR', this.mensajeGenerico);
+        });
+    } else {
+      this.maestroService.ConsultarTransportista({ Id: this.locId, Codigo: 'TransporteContratoPlantaSalida' })
+        .subscribe((res) => {
+          this.spinner.hide();
+          if (res.Result.Success) {
+            this.rowsTrans = res.Result.Data;
+          } else {
+            this.alertUtil.alertError('ERROR', res.Result.Message);
+          }
+        }, (err) => {
+          this.spinner.hide();
+          this.alertUtil.alertError('ERROR', this.mensajeGenerico);
+        });
+    }
+  }
+
+  AsignarTransportistas() {
+    if (this.selectedTrans && this.selectedTrans.length > 0) {
+      this.alertUtil.alertSiNoCallback('Pregunta',
+        '¿Está seguro de asignar a los transportistas seleccionados para el envio del café?',
+        () => {
+          this.spinner.show();
+          const request = {
+            transportistas: [],
+            Codigo: 'TransporteContratoPlantaSalida'
+          };
+          this.selectedTrans.forEach(x => {
+            request.transportistas.push({
+              IdProceso: this.locId,
+              TransporteId: x.TransporteId,
+              Usuario: this.userSession.NombreUsuario
+            })
+          });
+          this.contratoService.AsignarTransportistas(request)
+            .subscribe((res) => {
+              this.spinner.hide();
+              if (res.Result.Success) {
+                this.alertUtil.alertOkCallback('Confirmación',
+                  'Se ha asignado correctamente a los trasportistas seleccionados el traslado del café.',
+                  () => {
+                    this.ConsultarPorId();
+                  });
+              } else {
+                this.alertUtil.alertError('ERROR', res.Result.Message);
+              }
+            }, (err) => {
+              console.log(err);
+              this.spinner.hide();
+              this.alertUtil.alertError('ERROR', this.mensajeGenerico);
+            });
+        });
+    } else {
+      this.alertUtil.alertWarning('Validación', 'Seleccionar el/los transportista(s) que trasladarán el café.');
+    }
   }
 }
